eaa373ef22d6141a604eef97e84d76a210f8ab9e
scatler <38757410+scatler@users.noreply.github.com>
2019-08-02 00:28:37 +0300

    Deleted hashCode() and equals() from Trains and TrainsConfig

--- src/main/java/com/scatler/rrweb/entity/Trains.java
+++ src/main/java/com/scatler/rrweb/entity/Trains.java
@@ -64,31 +64,6 @@ public class Trains {
         this.trainId = trainId;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Trains trains = (Trains) o;
-
-        if (id != trains.id) return false;
-        if (locoId != null ? !locoId.equals(trains.locoId) : trains.locoId != null) return false;
-        if (name != null ? !name.equals(trains.name) : trains.name != null) return false;
-        if (seats != null ? !seats.equals(trains.seats) : trains.seats != null) return false;
-        if (trainId != null ? !trainId.equals(trains.trainId) : trains.trainId != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (locoId != null ? locoId.hashCode() : 0);
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (seats != null ? seats.hashCode() : 0);
-        result = 31 * result + (trainId != null ? trainId.hashCode() : 0);
-        return result;
-    }
 
 
     //-----------------------------------------
--- src/main/java/com/scatler/rrweb/entity/TrainsConfig.java
+++ src/main/java/com/scatler/rrweb/entity/TrainsConfig.java
@@ -41,27 +41,7 @@ public class TrainsConfig {
         this.trainId = trainId;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        TrainsConfig that = (TrainsConfig) o;
-
-        if (id != that.id) return false;
-        if (trCfgType != null ? !trCfgType.equals(that.trCfgType) : that.trCfgType != null) return false;
-        if (trainId != null ? !trainId.equals(that.trainId) : that.trainId != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (trCfgType != null ? trCfgType.hashCode() : 0);
-        result = 31 * result + (trainId != null ? trainId.hashCode() : 0);
-        return result;
-    }
 
     //---------------------------------------------
 
