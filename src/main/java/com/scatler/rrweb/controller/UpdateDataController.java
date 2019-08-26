b622918f806e658343034cfee227c29a1827c19f
scatler <38757410+scatler@users.noreply.github.com>
2019-08-26 23:52:50 +0300

    latest updates
    Took 25 minutes

--- src/main/java/com/scatler/rrweb/controller/UpdateDataController.java
+++ src/main/java/com/scatler/rrweb/controller/UpdateDataController.java
@@ -1,17 +1,21 @@
 package com.scatler.rrweb.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@RestController
+@RestController("webapi")
 public class UpdateDataController {
-
-    @RequestMapping("/foo")
-    void sendData(HttpServletResponse response) throws IOException {
-
+    @RequestMapping("/update")
+    public void sendData(HttpServletResponse response) throws IOException {
         response.sendRedirect("some-url");
+    }
+
+    @RequestMapping("/register/{stationId}")
+    public void registerListener(@PathVariable Integer stationId) {
+
     }
 }
