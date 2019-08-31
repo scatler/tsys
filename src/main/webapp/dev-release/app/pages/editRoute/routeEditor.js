2f9940f9cb40e3758a3d927c3932cae93542e5df
scatler <38757410+scatler@users.noreply.github.com>
2019-09-01 00:16:41 +0300

    before adding station table to route table

--- src/main/webapp/dev-release/app/pages/editRoute/routeEditor.js
+++ src/main/webapp/dev-release/app/pages/editRoute/routeEditor.js
@@ -6,11 +6,15 @@ angular.module('addressFormatter', []).filter('address', function () {
 });
 
 routeEditor.controller('routeEditCtrl', ['$scope', '$rootScope', '$http', '$q', '$interval', function ($scope, $rootScope, $http, $q, $interval, stationService) {
-        $scope.gridOptions = {};
+        var vm = this;
+
+        vm.gridOptions = {};
+        vm.stationGrid = {};
+
         $http({method: 'GET', url: 'http://localhost:8080/stations'}).then(function success(response) {
-            $scope.gridOptions.columnDefs[1].editDropdownOptionsArray = response.data;
+            vm.gridOptions.columnDefs[1].editDropdownOptionsArray = response.data;
         });
-        $scope.gridOptions.columnDefs = [
+        vm.gridOptions.columnDefs = [
             {name: 'routeId', enableCellEdit: false, width: '10%'},
             {
                 name: 'stationId', displayName: 'Station', editableCellTemplate: 'ui-grid/dropdownEditor', width: '20%',
@@ -25,13 +29,23 @@ routeEditor.controller('routeEditCtrl', ['$scope', '$rootScope', '$http', '$q', 
                 cellTemplate: '<button class="status-button btn btn-xs btn-danger" ng-click="grid.appScope.deleteRow(row)">Delete</button>'
             }
         ];
-        $scope.deleteRow = function (row) {
+
+        vm.stationGrid.columnDefs = [
+            {name: 'stationId', enableCellEdit: false, width: '10%'},
+            {name: 'name', enableCellEdit: true, width: '10%'},
+            {name: 'lineId', enableCellEdit: true, width: '10%'}
+        ]
+
+        vm.deleteRow = function (row) {
             console.log("Perform delete");
         };
-        $scope.saveRow = function (rowEntity) {
+
+
+
+        vm.saveRow = function (rowEntity) {
             // create a fake promise - normally you'd use the promise returned by $http or $resource
             var deferred = $q.defer();
-            $scope.gridApi.rowEdit.setSavePromise(rowEntity, deferred.promise);
+            vm.gridApi.rowEdit.setSavePromise(rowEntity, deferred.promise);
             $http({
                 method: 'PUT',
                 url: 'http://localhost:8080/saveRoute',
@@ -44,24 +58,22 @@ routeEditor.controller('routeEditCtrl', ['$scope', '$rootScope', '$http', '$q', 
             );
             return deferred.promise;
         };
-        $scope.gridOptions.onRegisterApi = function (gridApi) {
+
+
+
+        vm.gridOptions.onRegisterApi = function (gridApi) {
             //set gridApi on scope
             $scope.gridApi = gridApi;
-            gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+            gridApi.rowEdit.on.saveRow($scope, vm.saveRow);
         };
-        // Refresh the grid, calling the appropriate rest method.
-        $scope.refreshGrid = function () {
-            stationService.get(function (data) {
-                $scope.stations = data;
-            })
-        };
+
         $http.get('http://localhost:8080/routes')
             .then(function (response) {
                 var data = response.data;
                 for (i = 0; i < data.length; i++) {
                     data[i].registered = new Date(data[i].registered);
                 }
-                $scope.gridOptions.data = data;
+                vm.gridOptions.data = data;
             });
     }]
 )
