ad8b137aea95fbcab7266a49fd7cf47ce2c0d151
scatler <38757410+scatler@users.noreply.github.com>
2019-09-01 11:49:33 +0300

    add function to load data

--- src/main/webapp/dev-release/app/pages/editRoute/routeEditor.js
+++ src/main/webapp/dev-release/app/pages/editRoute/routeEditor.js
@@ -4,24 +4,35 @@ angular.module('addressFormatter', []).filter('address', function () {
         return input.street + ', ' + input.city + ', ' + input.state + ', ' + input.zip;
     };
 });
-
 routeEditor.controller('routeEditCtrl', ['$scope', '$rootScope', '$http', '$q', '$interval', function ($scope, $rootScope, $http, $q, $interval, stationService) {
         var vm = this;
-
         vm.gridOptions = {};
         vm.stationGrid = {};
 
+        function loadData () {
+
+        }
+
+        $http.get('http://localhost:8080/routes')
+        .then(function (response) {
+            vm.gridOptions.data = response.data;
+        });
         $http({method: 'GET', url: 'http://localhost:8080/stations'}).then(function success(response) {
             vm.gridOptions.columnDefs[1].editDropdownOptionsArray = response.data;
         });
+        
+        $http.get('http://localhost:8080/stations')
+            .then(function (response) {
+                vm.stationGrid.data = response.data;
+            });
         vm.gridOptions.columnDefs = [
             {name: 'routeId', enableCellEdit: false, width: '10%'},
             {
-                name: 'stationId', displayName: 'Station', editableCellTemplate: 'ui-grid/dropdownEditor', width: '20%',
+                name: 'stationId', displayName: 'Station', editableCellTemplate: 'ui-grid/dropdownEditor', width: '30%',
                 editDropdownValueLabel: 'name', editDropdownIdLabel: 'id', cellFilter: 'griddropdown:this'
             },
-            {name: 'arrivalTime', enableCellEdit: false, width: '10%'},
-            {name: 'stopMin', enableCellEdit: false, width: '10%'},
+            {name: 'arrivalTime', enableCellEdit: true, width: '10%'},
+            {name: 'stopMin', enableCellEdit: true, width: '10%'},
             {name: 'day', width: '10%', enableCellEdit: true},
             {
                 name: 'Delete',
@@ -29,22 +40,17 @@ routeEditor.controller('routeEditCtrl', ['$scope', '$rootScope', '$http', '$q', 
                 cellTemplate: '<button class="status-button btn btn-xs btn-danger" ng-click="grid.appScope.deleteRow(row)">Delete</button>'
             }
         ];
-
         vm.stationGrid.columnDefs = [
             {name: 'stationId', enableCellEdit: false, width: '10%'},
             {name: 'name', enableCellEdit: true, width: '10%'},
             {name: 'lineId', enableCellEdit: true, width: '10%'}
         ];
-
         vm.deleteRow = function (row) {
             console.log("Perform delete");
         };
-
-
-
         vm.saveRow = function (rowEntity) {
             var deferred = $q.defer();
-            $scope.gridApi.rowEdit.setSavePromise(rowEntity, deferred.promise);
+            vm.gridApi.rowEdit.setSavePromise(rowEntity, deferred.promise);
             $http({
                 method: 'PUT',
                 url: 'http://localhost:8080/saveRoute',
@@ -57,23 +63,12 @@ routeEditor.controller('routeEditCtrl', ['$scope', '$rootScope', '$http', '$q', 
             );
             return deferred.promise;
         };
-
-
-
         vm.gridOptions.onRegisterApi = function (gridApi) {
             //set gridApi on scope
-            $scope.gridApi = gridApi;
+            vm.gridApi = gridApi;
             gridApi.rowEdit.on.saveRow($scope, vm.saveRow);
         };
 
-        $http.get('http://localhost:8080/routes')
-            .then(function (response) {
-                var data = response.data;
-                for (i = 0; i < data.length; i++) {
-                    data[i].registered = new Date(data[i].registered);
-                }
-                vm.gridOptions.data = data;
-            });
     }]
 )
     .filter('griddropdown', function () {
